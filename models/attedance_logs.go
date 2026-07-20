76ed19dd3a9a7ca111528207f11d968634e75ce8
Fauzbyte <muhammadfauzanpratama420@gmail.com>
2026-07-20 22:25:19 +0700

    au  apa

--- models/attedance_logs.go
+++ models/attedance_logs.go
@@ -2,6 +2,8 @@ package models
 
 import "time"
 
+// Note: Untuk menghindari race condition, tabel ini membutuhkan unique index di database:
+// ALTER TABLE attedance_logs ADD UNIQUE INDEX uq_user_daily (user_id, (DATE(clock_in_time)));
 type AttedanceLogs struct {
 	ID          int64   `gorm:"primaryKey;autoIncrement"`
 	UserID      int64   `gorm:"index"`
